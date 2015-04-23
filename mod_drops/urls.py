0eefa733441358dfd2ae4801f199017b243cda0f
Henry Grantham <hgrantham@gmail.com>
2015-04-23 15:42:54 -0700

    create url for homepage

--- mod_drops/urls.py
+++ mod_drops/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'mod_drops.views.index', name='index'),
     # Examples:
     # url(r'^$', 'mod_drops.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
