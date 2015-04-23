bed8029452236ac1c277a53b00e603f3cd00874f
Henry Grantham <hgrantham@gmail.com>
2015-04-22 19:44:59 -0700

    added profile to admin panel

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,27 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
+from user_profile.models import UserProfile
 
-# Register your models here.
+
+class ProfileInLine(admin.TabularInline):
+    model = UserProfile
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Info', {'fields': ['username', 'first_name', 'last_name', 'email', 'password']}),
+        ('Status', {'fields': ['is_staff', 'is_active', 'is_superuser']}),
+        ('Date Information', {'fields': ['date_joined', 'last_login']}),
+        ('Permissions', {'fields': ['groups', 'user_permissions']}),
+    ]
+
+    readonly_fields = ('password', 'date_joined', 'last_login')
+    inlines = [
+        ProfileInLine
+    ]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile)
