b61dba40389e363f2c3d4f9db05d6e59f51a813f
Henry Grantham <hgrantham@gmail.com>
2015-04-26 23:58:06 -0700

    added urls to profile

--- mod_drops/urls.py
+++ mod_drops/urls.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'mod_drops.views.index', name='index'),
-    # Examples:
-    # url(r'^$', 'mod_drops.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^profile/', include('user_profile.urls'), namespace='profile'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
