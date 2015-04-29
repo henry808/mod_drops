b4c1f87fe9d161f532036f8a6ef36f4006da35b0
Henry Grantham <hgrantham@gmail.com>
2015-04-28 17:08:02 -0700

    added image to admin panel

--- image/admin.py
+++ image/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from imager_images.models import Image
 
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Info', {'fields': ['user', 'title', 'description', 'source', 'year', 'category']}),
+        ('Upload image', {'fields': ['picture']}),
+        ('Date information', {'fields': ['date_uploaded', 'date_modified', 'date_published']}),
+        ('Published', {'fields': ['published']}),
+
+    ]
+    readonly_fields = ('date_uploaded', 'date_modified', 'date_published')
+    list_display = ('user', 'title', 'picture', 'source', 'year', 'category', 'published', 'date_uploaded', 'date_modified', 'date_published', 'size')
+
+    def size(self, obj):
+        return obj.picture.size
+    size.short_description = 'File size'
+    size.admin_order = 'size'
