6669d1eb233a4bd1159076e8ae2cbc6174551cd5
Henry Grantham <hgrantham@gmail.com>
2015-05-08 17:13:17 -0700

    added disqus coments to site

--- mod_drops/settings.py
+++ mod_drops/settings.py
@@ -120,4 +120,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = 'tmp/activation'
 
 DISQUS_API_KEY = os.environ.get('DISQUS_API_KEY')
-DISQUS_WEBSITE_SHORTNAME = 'mod_drops'
+DISQUS_WEBSITE_SHORTNAME = 'moddrop'
