9f0c3d8d211e05d3d768c4edb2619ef95ad1d18a
Henry Grantham <hgrantham@gmail.com>
2015-05-12 16:29:06 -0700

    sort all displayed images by pk (chronological order entered)

--- image/disqus.py
+++ image/disqus.py
@@ -22,11 +22,17 @@ def get_disqus_sso(user):
     # generate our hmac signature
     sig = hmac.HMAC(DISQUS_SECRET_KEY, '%s %s' % (message, timestamp), hashlib.sha1).hexdigest()
 
-# return a script tag to insert the sso message
+    # return a script tag to insert the sso message
     return """<script type="text/javascript">
     var disqus_config = function() {
         this.page.remote_auth_s3 = "%(message)s %(sig)s %(timestamp)s";
         this.page.api_key = "%(pub_key)s";
+
+        this.sso = {
+          name:   "Mod Drops Title",
+          width:   "800",
+          height:  "400"
+    };
     }
     </script>""" % dict(
         message=message,
@@ -34,3 +40,9 @@ def get_disqus_sso(user):
         sig=sig,
         pub_key=DISQUS_PUBLIC_KEY,
     )
+
+    # use to customize button and icon
+    #       button:  "/test.gif",
+    #       icon:     "/favicon.png",
+    #       url:     "http://moddrops.com/login/",
+    #       logout:  "http://moddrops.com/logout/",
