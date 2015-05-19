8cca61864eaa61e8e6c5dd0b4d4c8962dfe2b324
Henry Grantham <hgrantham@gmail.com>
2015-05-18 17:57:19 -0700

    add base, dev, and prod configurations to settings

--- mod_drops/settings.py
+++ mod_drops/settings.py
@@ -15,110 +15,159 @@ import os
 
 import dj_database_url
 
+from configurations import Configuration
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+class Base(Configuration):
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qhpo$%3u2sr5uk++uuae4668$yp9k4j&sqzn!10-bbtop_ek-q'
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'qhpo$%3u2sr5uk++uuae4668$yp9k4j&sqzn!10-bbtop_ek-q'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
 
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'registration',
-    'user_profile',
-    'image',
-    'sorl.thumbnail',
-    'disqus',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'mod_drops.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'mod_drops/templates')]
-
-WSGI_APPLICATION = 'mod_drops.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "mod_drops/static/"),
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+    ROOT_URLCONF = 'mod_drops.urls'
 
-# registration
-ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window
-REGISTRATION_AUTO_LOGIN = True  # Automatically log the user in.
-LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
-LOGIN_URL = '/accounts/login/'
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'tmp/activation'
+    TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'mod_drops/templates')]
 
-DISQUS_SECRET_KEY = os.environ.get('DISQUS_SECRET_KEY')
-DISQUS_PUBLIC_KEY = os.environ.get('DISQUS_PUBLIC_KEY')
-DISQUS_WEBSITE_SHORTNAME = 'moddrop'
+    ROOT_URLCONF = 'mod_drops.urls'
+
+    WSGI_APPLICATION = 'mod_drops.wsgi.application'
+
+
+    # Database
+    # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+    DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "mod_drops/static/"),
+        )
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+    # registration
+    ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window
+    REGISTRATION_AUTO_LOGIN = True  # Automatically log the user in.
+    LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
+    LOGIN_URL = '/accounts/login/'
+
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = 'tmp/activation'
+
+    DISQUS_SECRET_KEY = os.environ.get('DISQUS_SECRET_KEY')
+    DISQUS_PUBLIC_KEY = os.environ.get('DISQUS_PUBLIC_KEY')
+    DISQUS_WEBSITE_SHORTNAME = 'moddrop'
+
+
+class Dev(Base):
+    DEBUG = True
+
+    TEMPLATE_DEBUG = True
+
+    THUMBNAIL_DEBUG = True
+
+    # Application definition
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'registration',
+        'user_profile',
+        'image',
+        'sorl.thumbnail',
+        'disqus',
+        'debug_toolbar',
+    )
+
+class Prod(Base):
+    DEBUG = True
+
+    TEMPLATE_DEBUG = True
+
+    THUMBNAIL_DEBUG = True
+
+    # Application definition
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'registration',
+        'user_profile',
+        'image',
+        'sorl.thumbnail',
+        'disqus',
+    )
+
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = 'tmp/activation'
+
+    ALLOWED_HOSTS = []
+
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 25
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.environ.get('HOST_PASSWORD', '')
+    DEFAULT_FROM_EMAIL = os.environ.get('HOST_USER', '')
