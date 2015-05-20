ac8470540f86bf2099c9187e47601cfba58c80ea
Henry Grantham <hgrantham@gmail.com>
2015-05-20 14:10:48 -0700

    Add secrets.yml to gitignore

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mod_drops.settings")
 
     from configurations.management import execute_from_command_line
 
