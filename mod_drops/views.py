322fba9ecc02cf2a421b7c842ab2a487e5c11f2b
Henry Grantham <hgrantham@gmail.com>
2015-06-24 12:39:52 -0700

    generate 4 random images and send to homepage

--- mod_drops/views.py
+++ mod_drops/views.py
@@ -3,13 +3,19 @@ from image.models import Image
 
 
 def index(request):
-    try:
-        random_image = Image.objects.order_by('?')[0]
-        # random_image = Image.objects.filter(
-        #     published='pub').order_by('?')[0].image.url
-    except IndexError:
-        random_image = "test"
-    context = {'random_image': random_image,
+    random_images = []
+    image_count = 4
+    while image_count > 0:
+        # does not work if less than 4 images in database
+        try:
+            random_images.append(Image.objects.order_by('?')[0])
+            # random_image = Image.objects.filter(
+            #     published='pub').order_by('?')[0].image.url
+        except IndexError:
+            random_images.append("test")
+        image_count -= 1
+    print random_images
+    context = {'random_images': random_images,
                'request': request}
     return render(request, 'index.html', context)
 
