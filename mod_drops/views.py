b8a309807a41b94bf455cbe41059ef9f825a2df1
Henry Grantham <hgrantham@gmail.com>
2015-06-23 17:42:20 -0700

    add random image to homepage, but doesn't work yet

--- mod_drops/views.py
+++ mod_drops/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from image.models import Image
 
 
 def index(request):
-
-    context = {'request': request}
+    try:
+        random_image = Image.objects.filter(
+            published='pub').order_by('?')[0].image.url
+    except IndexError:
+        random_image = "test"
+    context = {'random_image': random_image,
+               'request': request}
     return render(request, 'index.html', context)
+
 
 def about(request):
 
